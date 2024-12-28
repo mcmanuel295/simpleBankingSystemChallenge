d6e6a59fdf1db91664c44f52ce4c815a14088f2a
unknown <mcmanuel755@gmail.com>
2024-12-28 09:49:30 +0100

    start uo commit

---
+++ src/main/java/com/example/simpleBankingSystemChallenge/SimpleBankingSystemChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleBankingSystemChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBankingSystemChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBankingSystemChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/simpleBankingSystemChallenge/SimpleBankingSystemChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleBankingSystemChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBankingSystemChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
